f81270cbf47fed0d96d4cf748da1493707d0574c
Hasrat270 <hasrat3701@gmail.com>
2026-04-04 15:59:29 +0500

    style: forcefully hide Clerk signout button via appearance

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -139,7 +139,12 @@ export default function Sidebar() {
 
       {/* Simplified User Section */}
       <div className="pt-6 border-t border-border flex items-center gap-3 px-2">
-         <UserButton appearance={{ elements: { userButtonAvatarBox: "w-8 h-8 rounded-lg" } }}>
+         <UserButton appearance={{ 
+           elements: { 
+             userButtonAvatarBox: "w-8 h-8 rounded-lg",
+             userButtonPopoverActionButton__signOut: "hidden" 
+           } 
+         }}>
            <UserButton.MenuItems>
              <UserButton.Action label="manageAccount" />
            </UserButton.MenuItems>
